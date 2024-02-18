362500bd30c28b8a42aa035da2b4e7d4d62208cd
jordisorfer <jordisorfer1997@gmail.com>
2024-02-18 20:31:28 +0100

    Update practica_3.py

--- src/practica_3.py
+++ src/practica_3.py
@@ -1,4 +1,16 @@
+"""
+Aquest mòdul conté una funció per comprovar si un número és el factorial d'un altre.
+"""
 def es_factorial(entero,factorial):
+    """
+    Comprova si el factorial proporcionat és el factorial de l'enter donat.
+
+    :param entero: Un enter positiu.
+    :param factorial: El suposat factorial de l'enter.
+    :return: True si el factorial és correcte, False altrament.
+    :raises ValueError: Si els nombres no són positius.
+    :raises TypeError: Si els nombres no són enters.
+    """
     if isinstance(entero,int) and isinstance(factorial,int):
         if entero>0 and factorial>0:
             factorizacion=1
